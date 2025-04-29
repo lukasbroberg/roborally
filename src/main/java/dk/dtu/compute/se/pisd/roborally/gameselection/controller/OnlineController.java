b98b54d629c112fa0da16f18c0ddf925384fef42
LRBDeveloper <146123222+LRBDeveloper@users.noreply.github.com>
2025-04-29 09:01:27 +0200

    .

--- src/main/java/dk/dtu/compute/se/pisd/roborally/gameselection/controller/OnlineController.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/gameselection/controller/OnlineController.java
@@ -1,18 +1,60 @@
 package dk.dtu.compute.se.pisd.roborally.gameselection.controller;
 
 import dk.dtu.compute.se.pisd.roborally.controller.AppController;
+import dk.dtu.compute.se.pisd.roborally.gameselection.model.OnlineState;
+import dk.dtu.compute.se.pisd.roborally.gameselection.model.User;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.util.UriBuilder;
+
+import java.util.List;
 
 public class OnlineController {
 
+    private RestClient restClient = RestClient.builder().baseUrl("http://localhost:8080/roborally/").build();
     private AppController appController;
+    private OnlineState onlineState;
 
     public OnlineController(AppController appController) {
         this.appController = appController;
+        this.onlineState = new OnlineState();
     }
 
     public void signIn(String username){
-        //Todo: implement signup:
-        //Create new User with UID, username, etc.
+
+        //Query user, with username, from server:
+        try{
+            List<User> users = restClient.get().uri(uriBuilder -> uriBuilder
+                    .path("users/searchUsers").queryParam("name", username).build())
+                    .retrieve()
+                    .body(new ParameterizedTypeReference<List<User>>() {});
+
+            System.out.println(users);
+
+            //Create new user:
+            /*TODO: Use POST request to create new user on the server*/
+            if(users.isEmpty()){
+
+            }
+
+            //Sign user in:
+            /*TODO: Use POST request to create new user on the server*/
+
+
+
+        }catch(Exception err){
+            System.out.println(err.getMessage());
+        }
+
+
+
+        //restClient.post()
+
+
+
+
     }
 
     public void signOut(){
